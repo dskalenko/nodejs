a2e3786e2e3bae1f494a7a3af47bc55e0ae5d395
dskalenko <d.skalenko@gmail.com>
2015-07-28 00:18:28 +0300

    play with  modules

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
-var user = require('./user');
-var vasya = new user.User("Vasya");
-var petr = new  user.User("Petr");
-vasya.hello(petr);
+var User = require('./user');
+var db = require('db');
+var logger = require('./logger')(module);
+db.connect();
+function run() {
+    var vasya = new User("Vasya");
+    var petr = new User("Petr");
+    vasya.hello(petr);
+    logger(db.getPhrase("Run success"));
+}
+
+if(module.parent){
+    exports.run = run;
+}else{
+    run();
+}
--- user/index.js
+++ user/index.js
@@ -1,11 +1,15 @@
-var words = require('./ru');
+var db = require('db');
+var logger = require('../logger')(module);
 function User(name){
     this.name = name;
 }
 
 User.prototype.hello = function(who){
-    console.log(words.Hello + " " + who.name + " from " + this.name);
+    logger(db.getPhrase('Hello') + " " + who.name + " from " + this.name);
 };
 console.log("user js is require");
 
-exports.User = User;
+module.exports  = User;
+
+/*
+console.log(module);*/
